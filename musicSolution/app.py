89f3d91e46c9bcb648364a3d63d0076f648c226b
Denis <cph-dp153@cphbusiness.dk>
2023-05-24 08:36:08 +0200

    sat en masse på i streamlit, top 5 recommendations klar i frontend, samt execute algorithm med brugerens valg af input

--- musicSolution/app.py
+++ musicSolution/app.py
@@ -8,29 +8,58 @@ import matplotlib.pyplot as plt
 from data import load_user_artists, ArtistRetriever
 from recommender import ImplicitRecommender
 
+# Here user-artist matrix is loaded
+user_artist_matrix = load_user_artists(Path("../musicSolution/lastfmdata/user_artists.dat"))
+
+# Instantiate artist retriever:
+artist_retriever = ArtistRetriever()
+artist_retriever.load_artists(Path("../musicSolution/lastfmdata/artists.dat"))
 
 def main():
     st.title("Music Solution 2000")
+    st.header("More iterations = more precise algorithm.\nHowever the perfomance is affected too. Recommended max iterations=50")
 
-    # Here user-artist matrix is loaded
-    user_artist_matrix = load_user_artists(Path("../musicSolution/lastfmdata/user_artists.dat"))
+    # Get user input for factors, iterations, and regularization
+    factors = st.number_input("Factors", value=50)
+    iterations = st.number_input("Iterations", value=10)
+    regularization = st.number_input("Regularization", value=0.01)
 
-    # Instantiate artist retriever:
-    artist_retriever = ArtistRetriever()
-    artist_retriever.load_artists(Path("../musicSolution/lastfmdata/artists.dat"))
+    # Create a button to execute the algorithm
+    execute_algorithm = st.button("Execute Algorithm")
 
-    # Instantiate Alternating Least Square with implicit
-    implicit_model = implicit.als.AlternatingLeastSquares(factors=50, iterations=10, regularization=0.01)
+    if execute_algorithm:
+        # Instantiate Alternating Least Square with implicit using user input values
+        implicit_model = implicit.als.AlternatingLeastSquares(
+            factors=factors, iterations=iterations, regularization=regularization
+        )
 
-    # Instantiate recommender, fit, and recommend:
-    recommender = ImplicitRecommender(artist_retriever, implicit_model)
-    recommender.fit(user_artist_matrix)
-    artists, scores = recommender.recommend(2, user_artist_matrix, n=5)
+        # Instantiate recommender, fit, and recommend:
+        recommender = ImplicitRecommender(artist_retriever, implicit_model)
+        recommender.fit(user_artist_matrix)
+        artists, scores = recommender.recommend(2, user_artist_matrix, n=5)
 
-    # Display recommendations:
-    st.subheader("Top 5 recommendations for you")
-    for artist, score in zip(artists, scores):
-        st.write(f"{artist}: {score}")
+        # Display recommendations and plot side by side
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.subheader("Top 5 recommendations for you")
+            for artist, score in zip(artists, scores):
+                st.write(f"{artist}: {score}")
+
+        with col2:
+            # Create the plot
+            fig, ax = plt.subplots(figsize=(8, 6))
+            for artist, score in zip(artists, scores):
+                ax.bar(artist, score)
+
+            # Set up the plot properties
+            ax.set_xlabel("Artist")
+            ax.set_ylabel("Score")
+            ax.set_title("Top 5 recommendations")
+            ax.set_xticklabels(artists, rotation=45)
+
+            # Display the plot
+            st.pyplot(fig)
 
 
 if __name__ == "__main__":
