680ed16cace426c688f8e0f74d1f3d8b39fe76c2
Denis <cph-dp153@cphbusiness.dk>
2023-05-24 13:06:32 +0200

    lavet videre på det med at køre algoritmen på eget data

--- musicSolution/app.py
+++ musicSolution/app.py
@@ -40,7 +40,7 @@ def main():
 
     if execute_algorithm:
         # Append the user's favorite bands to user_artist_matrix
-        update_user_artists(user_id, session_state.input_user_artists, user_artist_matrix)
+        #update_user_artists(user_id, session_state.input_user_artists, user_artist_matrix)
 
         # Instantiate Alternating Least Square med implicit hvor der benyttes brugerens input-værdier:
         implicit_model = implicit.als.AlternatingLeastSquares(
@@ -75,8 +75,8 @@ def main():
             # Vis plot-tegningen
             st.pyplot(fig)
 
-    st.header("Instead of getting recommendations from another user, you can get your own recommendations. Write at least 5 artists and maximum 10 to get a precise recommendation.")
-    input_user_artist_likes = st.text_input("Write an artist you like and click 'Add'")
+    st.header("Instead of getting recommendations from another user, you can get your own recommendations. Write the name of at least 5 artists and maximum 10 to get a precise recommendation.")
+    input_user_artist_likes = st.text_input("Write the name of an artist you like and click 'Add'")
     if st.button("Add"):
         if len(session_state.input_user_artists) >= 10:
             st.write("You have reached the maximum limit of 10 artists.")
@@ -101,20 +101,33 @@ def main():
     if len(session_state.input_user_artists) >= 5:
         execute_algorithm_with_user_data = st.button("Execute Algorithm on your data", key="user_input_button")
         if execute_algorithm_with_user_data:
-            file_path = "user_artist_input_list.csv"
-            with open(file_path, "w", newline="") as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow(["Artist"])
-                writer.writerows([[artist] for artist in session_state.input_user_artists])
+            file_path = "./lastfmdata/user_artist_input_list.csv"
+            # Nu skal vi kontrollere hvad max_user_id er ud fra sidste linje i filen.
+            with open(file_path, "r") as csvfile:
+                lines = csvfile.readlines()
+                if lines:
+                    last_line = lines[-1].strip().split(" ")
+                    max_user_id = int(last_line[0])
+
+            # Gem listen i filen.
+            with open(file_path, "a", newline="") as csvfile:  # med "a" i stedet for "w" appendes der, så den ikke laver en ny liste hver gang.
+                writer = csv.writer(csvfile, delimiter=" ")
+                for i, artist in enumerate(session_state.input_user_artists, start=1):
+                    artist_id = i if i <= 10 else 10  # Limit artistID to a maximum of 10
+                    weight = 15000 - (1000 * (i - 1)) if i <= 10 else 5000  # Calculate weight based on position
+                    writer.writerow([max_user_id + 1, artist_id, weight])
 
             st.write("Your list was saved.")
 
-def update_user_artists(user_id, favorite_bands, user_artist_matrix):
-    for band in favorite_bands:
-        artist_id = get_artist_id(band)  # Retrieve the artist ID based on the band name
-        if artist_id is not None:
-            # Update the user_artist_matrix with the new entry
-            user_artist_matrix[user_id, artist_id] = 1  # Set the weight as 1
+            # Opdatér user_id, så der kan laves nye sammenligninger:
+            max_user_id += 1
+
+# def update_user_artists(user_id, favorite_bands, user_artist_matrix):
+#     for band in favorite_bands:
+#         artist_id = get_artist_id(band)  # Retrieve the artist ID based on the band name
+#         if artist_id is not None:
+#             # Update the user_artist_matrix with the new entry
+#             user_artist_matrix[user_id, artist_id] = 1  # Set the weight as 1
 
 
 def get_artist_id(artist_name):
