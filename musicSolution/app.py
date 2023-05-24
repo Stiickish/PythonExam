71f8e2043c289392b8f2dab542fed61094030422
Denis <cph-dp153@cphbusiness.dk>
2023-05-24 08:49:19 +0200

    lavet bruger input til user-id også

--- musicSolution/app.py
+++ musicSolution/app.py
@@ -17,18 +17,20 @@ artist_retriever.load_artists(Path("../musicSolution/lastfmdata/artists.dat"))
 
 def main():
     st.title("Music Solution 2000")
-    st.header("More iterations = more precise algorithm.\nHowever the perfomance is affected too. Recommended max iterations=50")
 
-    # Get user input for factors, iterations, and regularization
+    # Hent User-id via input feltet
+    user_id = st.number_input("User ID (min:2, max:2100)", min_value=2, max_value=2100, value=2, step=1)
+
+    # Hent user-input for de tre ting der skal med i algoritmen (factors,  iterations og regularization)
     factors = st.number_input("Factors", value=50)
     iterations = st.number_input("Iterations", value=10)
     regularization = st.number_input("Regularization", value=0.01)
 
-    # Create a button to execute the algorithm
+    # Button som kan starte algoritmen:
     execute_algorithm = st.button("Execute Algorithm")
 
     if execute_algorithm:
-        # Instantiate Alternating Least Square with implicit using user input values
+        # Instantiate Alternating Least Square med implicit hvor der benyttes brugerens input-værdier:
         implicit_model = implicit.als.AlternatingLeastSquares(
             factors=factors, iterations=iterations, regularization=regularization
         )
@@ -36,29 +38,29 @@ def main():
         # Instantiate recommender, fit, and recommend:
         recommender = ImplicitRecommender(artist_retriever, implicit_model)
         recommender.fit(user_artist_matrix)
-        artists, scores = recommender.recommend(2, user_artist_matrix, n=5)
+        artists, scores = recommender.recommend(user_id, user_artist_matrix, n=5)
 
-        # Display recommendations and plot side by side
+        # Laves to kolonner for at vise anbefalingerne side om side:
         col1, col2 = st.columns(2)
 
         with col1:
-            st.subheader("Top 5 recommendations for you")
+            st.subheader("Top 5 recommendations for user ID: " + str(user_id))
             for artist, score in zip(artists, scores):
                 st.write(f"{artist}: {score}")
 
         with col2:
-            # Create the plot
+            # Plot-tegningen er kolonne nummer 2
             fig, ax = plt.subplots(figsize=(8, 6))
             for artist, score in zip(artists, scores):
                 ax.bar(artist, score)
 
-            # Set up the plot properties
+            # Sæt labels for akserne og titlerne.
             ax.set_xlabel("Artist")
             ax.set_ylabel("Score")
             ax.set_title("Top 5 recommendations")
             ax.set_xticklabels(artists, rotation=45)
 
-            # Display the plot
+            # Vis plot-tegningen
             st.pyplot(fig)
 
 
